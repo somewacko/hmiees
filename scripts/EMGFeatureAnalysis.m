99d4a345776232a0e1c611c286d836935d44b974
zo7 <m.fly00@gmail.com>
2015-02-25 12:39:02 -0500

    Adds script to get statistics on features for an EMG signal

---
+++ scripts/EMGFeatureAnalysis.m
@@ -0,0 +1,81 @@
+% EMGFeatureAnalysis.m
+% Flynn, Michael
+%
+%   Reads in an EMG signal (signed 24-bit raw) and extracts features
+%   upon detecting onset of motion. 
+
+
+% Get information from the user
+
+filename        = input('File of raw EMG data : ', 's');
+sampling_rate   = input('Sampling rate (Hz)   : ');
+analysis_period = input('Analysis Period (s)  : ');
+
+samples_per_period = sampling_rate * analysis_period;
+
+
+
+% Read in the data
+
+file_id = fopen(filename, 'r');
+signal = fread(file_id, inf, 'bit24');
+signal = signal ./ 2^23; % Scale between -1 and 1
+
+t = 0 : 1/sampling_rate : (length(signal)-1)/sampling_rate;
+
+plot(t, signal);
+xlim([0 max(t)]); ylim([-1 1]);
+title('EMG Signal'); ylabel('t (s)');
+
+
+% Perform analysis on each frame
+
+onset_threshold = 0.05;
+
+mavslp_seg = 4;
+wamp_threshold = 0.05;
+zc_threshold = 0.05;
+
+mav    = [];
+mavslp = [];
+wamp   = [];
+zc     = [];
+
+x = samples_per_period;
+i = 0;
+
+while x < length(signal)
+
+    slice = signal(x-samples_per_period+1 : x);
+    
+    if max(abs(min(slice)),max(slice)) > onset_threshold
+        mav    = [mav,    mean_absolute_value(slice)]; %#ok<*AGROW> :)
+        mavslp = [mavslp, mean_absolute_value_slope(slice, mavslp_seg)];
+        wamp   = [wamp,   willison_amplitude(slice, wamp_threshold)];
+        zc     = [zc,     zero_crossings(slice, zc_threshold)];
+        i = i + 1;
+    end
+    
+    x = x + samples_per_period;
+end
+
+
+% Display results:
+
+fprintf('Parameters:\n\n');
+fprintf('MAVSLP Segments: %d\n', mavslp_seg);
+fprintf('WAMP Threshold:  %0.2f\n', wamp_threshold);
+fprintf('ZC Threshold:    %0.2f\n', zc_threshold);
+fprintf('Onset of motion method: Threshold @ %0.2f\n', onset_threshold);
+fprintf('\n');
+fprintf('Statistics:\n\n');
+fprintf('Num. Onsets: %d', i);
+fprintf('MAV:\n\tAvg: %f\n\tVar: %f\n\tStd: %f\n', ...
+    mean(mav), var(mav), std(mav));
+fprintf('MAVSLP:\n\tAvg: %f\n\tVar: %f\n\tStd: %f\n', ...
+    mean(mavslp), var(mavslp), std(mavslp));
+fprintf('WAMP:\n\tAvg: %f\n\tVar: %f\n\tStd: %f\n', ...
+    mean(wamp), var(wamp), std(wamp));
+fprintf('ZC:\n\tAvg: %f\n\tVar: %f\n\tStd: %f\n', ...
+    mean(zc), var(zc), std(zc));
+fprintf('\n');
