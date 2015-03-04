6ff01f297ca67cc4255693bffb3ca9ceca23ec0e
zo7 <m.fly00@gmail.com>
2015-03-04 13:09:01 -0500

    Removes filtering before decimation in get_emg_signal.m
    Matlab's decimate() function already filters the signal with a Chebyshev filter prior to decimation, which makes the Butterworth filter before it redundant. Also, there might be information in the lower band that we might want to keep, so one low-pass should be sufficient.

--- data/get_emg_signal.m
+++ data/get_emg_signal.m
@@ -9,25 +9,13 @@ function [emg_signal, t] = get_emg_signal(filename, sampling_rate)
     signal = signal ./ 2^23; % Scale between -1 and 1
 
 
-    % Filter and downsample the signal to 1000 Hz
-
-    % Note that the filtering stage might be unnecessary
-    % since Matlab's decimate() function uses an 8th-order
-    % Chebyshev filter before downsampling.
-
-    filtered_signal = detrend(signal);  % Remove DC offset
-    n = 4;                              % Order of the filter
-    nyquist = sampling_rate/2;
-    Wn = [10 500];                      % Cutoff frequencies
-
-    [btw_b, btw_a] = butter(n, Wn/nyquist);
-    filtered_signal = filtfilt(btw_b, btw_a, filtered_signal);
+    % Decimate the signal to 1000 Hz
 
     D = ceil(sampling_rate/1000);
-    emg_signal = decimate(filtered_signal, D);
+    emg_signal = decimate(signal, D);
     
     
-    % Get time vector too
+    % Also return the time vector
     
     t = 0 : 1/1000 : (length(emg_signal)-1)/1000;
     
