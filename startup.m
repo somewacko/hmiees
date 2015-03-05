c9e72f6ca5875b92f703762445ba548b792cc7c4
zo7 <m.fly00@gmail.com>
2015-03-05 16:41:09 -0500

    Adds startup.m to add classifier folders to the search path

---
+++ startup.m
@@ -0,0 +1,9 @@
+% Add folders to search path.
+
+addpath( strcat(pwd, '/classifier') );
+addpath( strcat(pwd, '/data'      ) );
+addpath( strcat(pwd, '/func'      ) );
+addpath( strcat(pwd, '/gui'       ) );
+addpath( strcat(pwd, '/scripts'   ) );
+
+disp('emg-classifier folders added to search path.');
