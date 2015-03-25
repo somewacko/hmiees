81a145caf6d130c35e65c849d4f8f0fbc555506b
zo7 <m.fly00@gmail.com>
2015-03-24 20:56:07 -0400

    Makes FeatureAnalysis a bit more dynamic

--- scripts/EMGFeatureAnalysis.m
+++ scripts/EMGFeatureAnalysis.m
@@ -31,67 +31,55 @@ fprintf('Sampling period: %f\n', sampling_period);
 fprintf('\n');
 
 
-% Calculate MAV and WL (these don't have parameters)
+% Read in signals
 
-feat_mav = cell(1, size(directories, 2));
-feat_ssi = cell(1, size(directories, 2));
-feat_var = cell(1, size(directories, 2));
-feat_wl  = cell(1, size(directories, 2));
+signals = cell(1, size(directories, 2));
 
 for i = 1:size(directories, 2)
     directory = char(directories(i));
-       
-    signals = get_emg_group(directory, file_sampling_rate);
-    slice = signals(1:sampling_period,:);
-    
-    feat_mav(i) = {EMGFeature.MAV.extract(slice, [])};
-    feat_ssi(i) = {EMGFeature.SSI.extract(slice, [])};
-    feat_var(i) = {EMGFeature.VAR.extract(slice, [])};
-    feat_wl(i)  = {EMGFeature.WL.extract(slice, [])};
+        
+    sig = get_emg_group(directory, file_sampling_rate);
+    signals(i) = {sig(1:sampling_period,:)};
 end
 
-fprintf('%s:\n', EMGFeature.MAV.name);
-for i = 1:size(directories, 2)
-    fprintf('\t%s\n', char(names(i)));
-    fprintf('\t\tMean: %f\n', mean( cell2mat(feat_mav(i)) ));
-    fprintf('\t\tStd:  %f\n',  std( cell2mat(feat_mav(i)) ));
+
+% Calculate features without parameters
+
+feats = [ ...
+    EMGFeature.MAV, ...
+    EMGFeature.SSI, ...
+    EMGFeature.VAR, ...
+    EMGFeature.WL   ...
+];
+
+for i = 1:length(feats)
+    
+    calc = cell(1, size(directories, 2));
+    
+    for l = 1:size(directories, 2)
+        calc(l) = {feats(i).extract(cell2mat(signals(l)), [])};
+    end
+    
+    fprintf('%s:\n', feats(i).name);
+    for l = 1:size(directories, 2)
+        fprintf('\t%s\n', char(names(l)));
+        fprintf('\t\tMean: %f\n', mean( cell2mat(calc(l)) ));
+        fprintf('\t\tStd:  %f\n',  std( cell2mat(calc(l)) ));
+    end
 end
-fprintf('%s:\n', EMGFeature.SSI.name);
-for i = 1:size(directories, 2)
-    fprintf('\t%s\n', char(names(i)));
-    fprintf('\t\tMean: %f\n', mean( cell2mat(feat_ssi(i)) ));
-    fprintf('\t\tStd:  %f\n',  std( cell2mat(feat_ssi(i)) ));
-end
-fprintf('%s:\n', EMGFeature.VAR.name);
-for i = 1:size(directories, 2)
-    fprintf('\t%s\n', char(names(i)));
-    fprintf('\t\tMean: %f\n', mean( cell2mat(feat_var(i)) ));
-    fprintf('\t\tStd:  %f\n',  std( cell2mat(feat_var(i)) ));
-end
-fprintf('%s:\n', EMGFeature.WL.name);
-for i = 1:size(directories, 2)
-    fprintf('\t%s\n', char(names(i)));
-    fprintf('\t\tMean: %f\n', mean( cell2mat(feat_wl(i)) ));
-    fprintf('\t\tStd:  %f\n',  std( cell2mat(feat_wl(i)) ));
-end
-fprintf('\n');
 
 
 % Plot WAMP and ZC
 
-fig_wamp = figure(1);
+fig_wamp = figure(1); clf;
 title('Willison Amplitude');
 xlabel('Parameter Value');
 
-fig_zc = figure(2);
+fig_zc = figure(2); clf;
 title('Zero Crossings');
 xlabel('Parameter Value');
 
 for i = 1:size(directories, 2)
-    directory = char(directories(i));
-       
-    signals = get_emg_group(directory, file_sampling_rate);
-    slice = signals(1:sampling_period,:);
 
     % WAMP
     
@@ -101,7 +89,7 @@ for i = 1:size(directories, 2)
     stds = zeros(1, length(val));
     
     for n = 1:length(val)
-        wamp = EMGFeature.WAMP.extract(slice, val(n));
+        wamp = EMGFeature.WAMP.extract(cell2mat(signals(i)), val(n));
         avgs(n) = mean(wamp);
         stds(n) = std(wamp); 
     end
@@ -117,7 +105,7 @@ for i = 1:size(directories, 2)
     stds = zeros(1, length(val));
     
     for n = 1:length(val)
-        wamp = EMGFeature.ZC.extract(slice, val(n));
+        wamp = EMGFeature.ZC.extract(cell2mat(signals(i)), val(n));
         avgs(n) = mean(wamp);
         stds(n) = std(wamp); 
     end
