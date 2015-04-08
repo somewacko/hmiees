9f3f14c546391cf9f115f1783e82cb05844bfa02
zo7 <m.fly00@gmail.com>
2015-04-08 13:40:36 -0400

    Adds filter to apply low-pass of 500 Hz from 8000 Hz

--- cemg/emg_processing.c
+++ cemg/emg_processing.c
@@ -47,16 +47,57 @@ processing_info_t init_processing_info(unsigned speriod)
 
 void read_in_sample(processing_info_t * processing_info, emg_sample_t sample)
 {
-    unsigned i = processing_info->current_index + 1 >= BUFFER_SIZE ?
+    // Determine the current index in the buffer and put in input buffer
+
+    unsigned buffer_index = processing_info->current_index + 1 >= BUFFER_SIZE ?
         0 : processing_info->current_index + 1;
 
-    processing_info->input_buffer[i] = sample;
+    processing_info->input_buffer[buffer_index] = sample;
 
-    // Do filtering, etc. For now output = input
-    processing_info->output_buffer[i] = sample;
+
+    // 7th-order butterworth filter coefficients to get cutoff frequency
+    // of 500 Hz from 8000 Hz
+
+    // ---- matlab:
+    // 
+    // > [b, a] = butter(7, 0.25);
+    // > a =
+    //       1.0000   -3.4815    5.7112   -5.5169    
+    //       3.3461   -1.2626    0.2729   -0.0259
+    // > b =
+    //       0.0003    0.0024    0.0071    0.0118
+    //       0.0118    0.0071    0.0024    0.0003
+
+    const unsigned filt_size = 8;
+    const float a[] = {  1.0000, -3.4815,  5.7112, -5.5169,
+                         3.3461, -1.2626,  0.2729, -0.0259 };
+    const float b[] = {  0.0003,  0.0024,  0.0071,  0.0118,
+                         0.0118,  0.0071,  0.0024,  0.0003 };
+
+
+    // Indicies from current index down without going out of bounds
+    // (eg: 3, 2, 1, 0, 7, 6...)
+
+    unsigned z[BUFFER_SIZE]; 
+    for (int i = 0; i < BUFFER_SIZE; i++)
+        z[i] = (-i + BUFFER_SIZE + buffer_index) % BUFFER_SIZE;
+
+
+    // Apply filter
+
+    emg_sample_t filtered_sample = 0;
+
+    for (unsigned i = 0; i < filt_size; i++)
+        filtered_sample += b[i] * processing_info->input_buffer [ z[i] ];
+    for (unsigned i = 1; i < filt_size; i++)
+        filtered_sample -= a[i] * processing_info->output_buffer[ z[i] ];
+
+    processing_info->output_buffer[buffer_index] = filtered_sample;
+
 
     // Update current index after processing is complete
-    processing_info->current_index = i;
+
+    processing_info->current_index = buffer_index;
 }
 
 
@@ -98,6 +139,9 @@ void process_sample(processing_info_t * processing_info)
     static unsigned total_count;
 
     emg_sample_t sample = get_current_sample(processing_info);
+
+    if (total_count < 10)
+        printf("%d - Read in %7.4f\n", total_count, sample);
 
     switch (state)
     {
